1cb3dbe472bc71c3e2669009a25a22cbf4fd765f
Diego <diego.gamboaa03@gmail.com>
2022-06-04 14:17:56 -0400

    Creando mainActivity

--- app/src/main/java/com/example/farmacosjava/MainActivity.java
+++ app/src/main/java/com/example/farmacosjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.farmacosjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickTextView(View view){
+        Toast.makeText(getApplicationContext(), "Remplazar por tu codigo", Toast.LENGTH_LONG)
+                .show();
+    }
+
+    public void clickNextButton(View view){
+        Toast.makeText(getApplicationContext(), "Remplazar por tu codigo", Toast.LENGTH_LONG)
+                .show();
     }
 }
